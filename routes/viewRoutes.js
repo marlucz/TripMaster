8966c486d4b8abc585471fdb884aac04a8019651
Bukacz <mbukacz@gmail.com>
2019-10-15 23:33:52 +0200

    scss basic setup, build signup form and first styles

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-    res.status(200).render('layout', {
+    res.status(200).render('signup', {
         title: 'TripMaster'
     })
 });
