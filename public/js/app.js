d302782f78d3b0bd6d1af841c8ef026a83f8c135
Bukacz <mbukacz@gmail.com>
2019-11-09 15:09:00 +0100

    contentHeight helper module to set proper content height value according to nav heights

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
 import '../sass/style.scss';
 
+//  ****************  MODULE IMPORTS **************
 import timelineDescriptionAccordion from './modules/timelineDescriptionAccordion';
+import contentHeight from './modules/contentHeight';
 
 //  ****************  DOM ELEMENTS **************
 const chevrons = document.querySelectorAll('.chevron');
 
 // *****************  DOM MANIPULATION *************
 
-// itinerary timeline description show and hide
+// content height dynamic height based on nav heights
+let contentListeners = ['DOMContentLoaded', 'resize'];
 
+contentListeners.forEach(listener =>
+  window.addEventListener(listener, contentHeight)
+);
+
+// itinerary timeline description show and hide
 chevrons.forEach(chevron => {
   chevron.addEventListener('click', timelineDescriptionAccordion);
 });
---
+++ public/js/modules/contentHeight.js
@@ -0,0 +1,18 @@
+function contentHeight() {
+  const navTop = document.querySelector('.nav--top');
+  const navBottom = document.querySelector('.nav--bottom');
+
+  const navTopHeight = navTop.getBoundingClientRect().height;
+  const navBottomHeight = navBottom
+    ? navBottom.getBoundingClientRect().height
+    : 0;
+
+  let { innerHeight } = window;
+
+  document.documentElement.style.setProperty(
+    '--content',
+    `${innerHeight - navTopHeight - navBottomHeight}px`
+  );
+}
+
+export default contentHeight;
