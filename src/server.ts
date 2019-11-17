f4dd30d3537f45f0c7c00166275b89509bc0dc29
Bukacz <mbukacz@gmail.com>
2019-11-17 15:36:58 +0100

    set environmental parameters

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import app from './app';
 import { Request, Response, NextFunction } from 'express';
-const PORT = 3000;
+import { resolve } from 'path';
+import { config } from 'dotenv';
+
+config({ path: resolve(__dirname, "variables.env") })
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello From The TypeScript Server');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
 });
