5dc3e1542347c557b58c02158577cf78d9ef0005
marlucz <marcin_luczak@hotmail.com>
2020-05-27 20:24:56 +0200

    small refactor itineraryController

--- src/controllers/itineraryController.ts
+++ src/controllers/itineraryController.ts
@@ -9,33 +9,33 @@ class ItineraryController {
    *
    */
   public addItinerary: RequestHandler = async (req, res) => {
+    console.log(req.params.slug, req.body.userID);
     const tripBySlug = await Trip.findOne({
       slug: req.params.slug,
-      userID: req.query.userID
+      userID: req.body.userID
     });
+
     if (tripBySlug) {
-      req.body.tripID = tripBySlug._id;
-    }
+      const newItinerary = {
+        name: req.body.name,
+        location: {
+          type: 'Point',
+          coordinates: [req.body.lng, req.body.lat],
+          address: req.body.location
+        },
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        userID: req.body.userID,
+        tripID: tripBySlug._id
+      };
 
-    const newItinerary = {
-      name: req.body.name,
-      location: {
-        type: 'Point',
-        coordinates: [req.body.lng, req.body.lat],
-        address: req.body.location
-      },
-      startDate: req.body.startDate,
-      endDate: req.body.endDate,
-      userID: req.query.userID,
-      tripID: req.body.tripID
-    };
-
-    try {
-      await new Itinerary(newItinerary).save((err, itinerary) => {
-        res.status(200).send(itinerary);
-      });
-    } catch (err) {
-      res.sendStatus(500);
+      try {
+        await new Itinerary(newItinerary).save((err, itinerary) => {
+          return res.status(200).send(itinerary);
+        });
+      } catch (err) {
+        res.sendStatus(500);
+      }
     }
   };
   /**
@@ -47,8 +47,9 @@ class ItineraryController {
       slug: req.params.slug,
       userID: req.query.userID
     });
+
     if (tripBySlug) {
-      Itinerary.find({ userID: req.query.userID, tripID: tripBySlug._id })
+      await Itinerary.find({ userID: req.query.userID, tripID: tripBySlug._id })
         .then(results => {
           res.status(200).send(results);
         })
